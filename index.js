bd95f3b094ce2459f4b2dca0df81df3eb2687eb6
Umaid Ismail <umaidismail@umaids-air.mynetworksettings.com>
2024-10-09 09:54:16 -0700

    add a variable message

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+// create variables that don't say the combination but add up, divide, and subtract
+//  to eachother
+// add an alert message with a string and variables that tell you the combination code
+const message =`Your combination is`;
 const comb1 = 5 + 5; 
 const comb2 = 80 / 2;
 const comb3 = 40 - 1;
 
-alert(`Your combination is ${comb1} ${comb2} ${comb3}`);
+alert(`${message} ${comb1} ${comb2} ${comb3}`);
