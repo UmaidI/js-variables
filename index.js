bb37fd9c7a6963f7a40fa2148e823fe9ced95b9a
Umaid Ismail <umaidismail@umaids-air.mynetworksettings.com>
2024-10-09 09:57:37 -0700

    change the message variable

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // create variables that don't say the combination but add up, divide, and subtract
 //  to eachother
 // add an alert message with a string and variables that tell you the combination code
-const message =`Your combination is`;
+const message =`You have received this message because you have been chosen to open 
+an important vault. Here is the secret combination:`;
 const comb1 = 5 + 5; 
 const comb2 = 80 / 2;
 const comb3 = 40 - 1;
