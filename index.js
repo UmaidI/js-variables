f240918a18265abc8c32266ddae5464b5c7a6199
Umaid Ismail <umaidismail@umaids-air.mynetworksettings.com>
2024-10-09 09:45:06 -0700

    add semicolon at end of line 5

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const comb1 = 5 + 5;
 const comb2 = 80 / 2;
 const comb3 = 40 - 1;
 
-alert(`Your combination is ${comb1} ${comb2} ${comb3}`)
+alert(`Your combination is ${comb1} ${comb2} ${comb3}`);
